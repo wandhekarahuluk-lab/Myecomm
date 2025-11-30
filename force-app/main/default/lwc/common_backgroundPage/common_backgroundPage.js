d81c2c7c7c91068156a576180a9fd126bfc8bba2
Rahul <wandhekarahuluk@gmail.com>
2025-11-30 19:54:04 +0000

    adding SF project to local repo

--- force-app/main/default/lwc/common_backgroundPage/common_backgroundPage.js
+++ force-app/main/default/lwc/common_backgroundPage/common_backgroundPage.js
@@ -1,3 +1,10 @@
-import { LightningElement } from 'lwc';
 
-export default class Common_backgroundPage extends LightningElement {}
+import { LightningElement } from 'lwc';
+import common_backgroundimage from '@salesforce/resourceUrl/common_backgroundimage';
+
+export default class CommonBackgroundPage extends LightningElement {
+    backgroundStyle = `background-image: url(${common_backgroundimage});
+                       background-size: cover;
+                       background-position: center;
+                       background-repeat: no-repeat;`;
+}
